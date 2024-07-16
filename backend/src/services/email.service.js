7babbe4be14f48aff0ed5f1dada2f01d64bbc4f2
katya <0303gavkat@gmail.com>
2024-07-16 10:21:41 -0600

    deleted local files

---
+++ backend/src/services/email.service.js
@@ -0,0 +1,11 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+  },
+})
