3c543f0a5e0b584a818cefc374a685ad4fddbcfb
katya <0303gavkat@gmail.com>
2024-07-17 14:22:07 -0600

    send activation email

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import { user } from '../models/user.js';
+import { emailService } from '../services/email.service.js';
+import {v4 as uuidv4} from 'uuid'
 
 const register = async (req, res) => {
   const { email, password } = req.body;
 
   const newUser = await user.create({ email, password });
+  const token = uuidv4()
+  await emailService.sendActivationEmail(email, token)
   res.send(newUser);
 };
 
--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,5 +1,5 @@
-import nodemailer from 'nodemailer'
-import 'dotenv/config'
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
@@ -8,15 +8,35 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-})
+});
 
+function send({ email, subject, html }) {
+  return transporter.sendMail({
+    to: email,
+    subject,
+    html,
+  });
+}
 
+function sendActivationEmail(email, token) {
+  const href = `${process.env.CLIENT_HOST}/activate/${token}`
+  const html = `
+  <h1>Activate account</h1>
+  <a href="${href}">${href}</a>
+`
+  return send({ email, html, sunbect: 'Activate' })
+}
 
 const info = await transporter.sendMail({
-    to: "fevah39249@reebsd.com",
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  to: 'fevah39249@reebsd.com',
+  subject: 'Hello ✔', // Subject line
+  text: 'Hello world?', // plain text body
+  html: '<b>Hello world?</b>', // html body
+});
 
-  console.log("Message sent: %s", info.messageId);
+console.log('Message sent: %s', info.messageId);
+
+export const emailService = {
+  sendActivationEmail,
+  send
+}
